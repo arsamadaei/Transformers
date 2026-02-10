035e03a17e79471f8cbd6cdb2b08db309b1a0824
Polkadot <aryanteams2019@gmail.com>
2026-02-09 17:23:13 -0800

    Finalized processes.py
    I completed and debugged processes.py.
    For some reason, translate.py was removed, so I will re-add it to the repository.

--- processes.py
+++ processes.py
@@ -1,3 +1,5 @@
+import random
+
 class Mindmap:
     def __init__(self, map: dict):
         """
@@ -16,29 +18,66 @@ class Mindmap:
         """
         Returns nodes connected to the edges of a source node.
         """
-
-        return map[node]
+        if node in self.map:
+            return self.map[node]
+        else:
+            return []
 
     def connected(self, srcNode, node) -> bool:
-        if node in map[srcNode]:
+        if srcNode in self.map and node in self.map[srcNode]:
             return True
+        return False
 
-        else: return False
+    def connect(self,srcNode, node, direction: bool | None =None):
+        """
+        If direction is False: srcNode to node
+        If direction is True: node to srcNode
+        If direction is None: bidirectional connection
+        """
+        if direction is None or direction is True:
+            if node not in self.map:
+                self.map[node] = []
+
+            self.map[node].append(srcNode)
+
+        if direction is None or direction is False:
+            if srcNode not in self.map:
+                self.map[srcNode] = []
+
+            self.map[srcNode].append(node)
 
 
 class TimeStamp:
     def __init__(self, _init: int, _term: int, _dis=None):
-        # _dis = {"type": TimeStamp}
+        """
+        _init: time of initiation.
+        _term: time of termination.
+        _dis: disruptions such as pausing a task, or async pausing.
+        _dis = {"type": TimeStamp}
+        """
         self.ts = {"initialized": _init, "terminated": _term}
-        
-        if _dis:
-            self.ts["disruptions"] = []
+        self.ts["disruptions"] = []
+    
+        if _dis is not None:
             for i in _dis:
-                self.ts["disruptions"][i] = _dis[i]
+                self.ts["disruptions"].append({i: _dis[i]})
+
+    def add_disruption(self, _type: str, timeStamp: "TimeStamp"):
+        self.ts["disruptions"].append({_type: timeStamp})
 
 
-class Process:
-    def __init__(self, name: "str", timeline: TimeStamp):
+class Process(TimeStamp):
+    def __init__(self, name: str, layer: int, _init: int, _term: int, _dis=None):
+        super().__init__(_init, _term, _dis)
         self.name = name
-        slef.timeline = timeline
+
+        # Generate process ID
+        hex_chars = '0123456789abcdef'
+        randID = ''.join(random.choices(hex_chars, k=4))
+
+        self.uid = f"{layer}-{randID}"
+
+        
+        
+
 
